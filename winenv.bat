efd300d53af2112016b0dac298e7ae9195b3e057
kajih <kajih@mimiru>
2024-04-14 14:42:15 +0200

    vcpkg/cmake windows env

---
+++ winenv.bat
@@ -0,0 +1,6 @@
+set VCPKG_DEFAULT_TRIPLET=x64-windows-static-md
+
+set OPENCV_LINK_LIBS=Gdi32.lib,User32.lib,Comdlg32.lib
+set OPENCV_LINK_PATHS=D:/DevKits/vcpkg/installed/x64-windows-static-md/lib,D:/DevKits/vcpkg/packages/opencv4_x64-windows-static-md/debug/lib
+set OpenCV_DIR=d:/DevKits/vcpkg/x64-windows-static-md/share/opencv4
+
