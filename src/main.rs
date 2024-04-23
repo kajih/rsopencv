7f92cc3f670e279c8a65260cf60a716682fb8be2
Johan Häggqvist <kajih@GUNGNIR.lan>
2024-04-23 20:09:56 +0200

    added webcam

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,26 @@
-use opencv::prelude::*;
-use opencv::{highgui, imgcodecs, Result};
 use opencv::core::get_version_string;
+use opencv::prelude::*;
+use opencv::{highgui, imgcodecs, videoio, Result};
 
 fn main() -> Result<()> {
     let foo = get_version_string();
 
     println!("V {:?}", foo);
-    
-    let image = imgcodecs::imread("Ankha.jpg", 0)?;
     highgui::named_window("hello opencv!", 0)?;
+    let image = imgcodecs::imread("Ankha.jpg", 0)?;
     highgui::imshow("hello opencv!", &image)?;
-    highgui::wait_key(10000)?;
 
+    let mut cam = videoio::VideoCapture::new(0, videoio::CAP_ANY)?;
+    let mut frame = Mat::default(); // This array will store the web-cam data
+
+    loop {
+        cam.read(&mut frame)?;
+        highgui::imshow("window", &frame)?;
+        let key = highgui::wait_key(1)?;
+        if key == 113 {
+            // quit with q
+            break;
+        }
+    }
     Ok(())
 }
-
